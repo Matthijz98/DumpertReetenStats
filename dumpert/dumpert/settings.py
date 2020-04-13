0b967ae5832e0d1ce39f5061714cf0ec0efb3bf0
Matthijs Zondervan <mzondervan@hotmail.nl>
2020-04-13 20:59:35 +0200

    get ready for google cloud

--- dumpert/dumpert/settings.py
+++ dumpert/dumpert/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -135,6 +134,3 @@ THUMBNAIL_PROCESSORS = (
     'filer.thumbnail_processors.scale_and_crop_with_subject_location',
     'easy_thumbnails.processors.filters',
 )
-
-# Activate Django-Heroku.
-django_heroku.settings(locals())
