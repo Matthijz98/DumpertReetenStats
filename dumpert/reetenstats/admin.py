b78367a2c2721781b39eb953c5e5ded6d7be8966
Matthijs Zondervan <mzondervan@hotmail.nl>
2020-04-19 20:22:07 +0200

    made the Show admin usable
    added the inline admin
    added auto complete fields

--- dumpert/reetenstats/admin.py
+++ dumpert/reetenstats/admin.py
@@ -1,9 +1,31 @@
 from django.contrib import admin
 from .models import Show, Gast, RatingType, Rating, Video
 
+
+class VideoAdmin(admin.ModelAdmin):
+    search_fields = ["video_tile"]
+
+
+class RatingTypeAdmin(admin.ModelAdmin):
+    search_fields = ["rating_name"]
+
+class GastAdmin(admin.ModelAdmin):
+    search_fields = ["gast_name", "rating_type"]
+
+
+class RatingAdmin(admin.StackedInline):
+    model = Rating
+    autocomplete_fields = ["rating_by", "rating_type"]
+
+
+class ShowAdmin(admin.ModelAdmin):
+    model = Show
+    inlines = [RatingAdmin]
+
+
 # Register your models here.
-admin.site.register(Show)
-admin.site.register(RatingType)
+admin.site.register(Show, ShowAdmin)
+admin.site.register(RatingType, RatingTypeAdmin)
 admin.site.register(Rating)
-admin.site.register(Video)
-admin.site.register(Gast)
+admin.site.register(Video, VideoAdmin)
+admin.site.register(Gast, GastAdmin)
