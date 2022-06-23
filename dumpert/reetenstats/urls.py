41420451e335384691ebd5eb3d274a715f4f3a50
Matthijs Zondervan <mzondervan@hotmail.nl>
2022-06-23 23:41:35 +0200

    remove some more not needed JSON stuff

--- dumpert/reetenstats/urls.py
+++ dumpert/reetenstats/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.showsview),
     path('show/<int:show_id>', views.showview),
-    path('api/json/ratingsinshow', views.ratinginfojsonview),
     path('ads.txt', views.adsview),
     path('page/over', views.aboutview, name='about'),
     path('stats/top10', views.top10view, name="top10"),
--- dumpert/reetenstats/views.py
+++ dumpert/reetenstats/views.py
@@ -12,34 +12,11 @@ def showsview(request):
 
 
 def showview(request, show_id):
-    data = {"show": Show.objects.get(id=show_id)}
+    data = {"show": Show.objects.get(id=show_id),
+            "videos": Video.objects.all().filter(rating__rating_in_show_id=show_id).distinct()}
     return render(request=request,
                   template_name='reetenstats/show.html',
                   context=data)
-
-
-def ratinginfojsonview(request,):
-    show_id = request.GET.get('show')
-    videos = list(dict.fromkeys(Video.objects.all().filter(rating__rating_in_show=show_id)))
-    results = []
-
-    for video in videos:
-        ratings = Rating.objects.all().filter(rating_in_show=show_id).filter(rating_video=video.id).exclude(rating_type=0)
-        ratings_in_video = []
-        for rating in ratings:
-            ratings_in_video.append({"by":rating.rating_by.gast_name,
-                                     "rating_amount": str('%g'%(rating.rating_ammount)),
-                                     "rating_type": rating.rating_type.rating_type_name})
-
-        results.append({
-            "title": video.video_title,
-            "description": video.video_description,
-            "dumpert_id": video.video_dumpert_id,
-            "ratings": ratings_in_video
-        })
-    data = json.dumps(results)
-    mimetype = 'application/json'
-    return HttpResponse(data, mimetype)
 
 
 def adsview(request):
